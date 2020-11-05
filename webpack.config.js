c17f3905eecff2fd918453252ae94b7442e18c7b
Tasty <sapegin.petya@yandex.ru>
2020-11-05 16:25:28 +0400

    refactor: add webpack

---
+++ webpack.config.js
@@ -0,0 +1,43 @@
+const path = require('path');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const loader = require('sass-loader');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+
+module.exports = {
+    entry: './gem-puzzle/src/app.js',
+    output: {
+        path: path.resolve(__dirname, 'gem-puzzle/dist'),
+        filename: 'bundle.js'
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+                template: './gem-puzzle/src/index.html'
+            }
+
+        ),
+        new MiniCssExtractPlugin({
+            filename: 'style.css',
+        })
+    ],
+    module: {
+        rules: [{
+            test: /\.scss$/,
+            use: [{
+                    loader: MiniCssExtractPlugin.loader,
+                    options: {
+                        reloadAll: true
+                    },
+                },
+                'css-loader',
+                'sass-loader'
+            ]
+        }]
+    },
+    devServer: {
+        port: 3000,
+        open: true,
+        overlay: true
+    }
+};
