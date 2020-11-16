a992f9abdc61a3cdcc7ce1fcb62822d8de2d4897
Tasty <sapegin.petya@yandex.ru>
2020-11-16 21:22:24 +0400

    refactor: change paths to files

--- gem-puzzle/src/js/field.js
+++ gem-puzzle/src/js/field.js
@@ -16,7 +16,7 @@ export default class Field {
         this.time = document.createElement('div');
         this.moves = document.createElement('div');
 
-        this.audio.src = '/gem-puzzle/assets/sounds/moving.mp3';
+        this.audio.src = './gem-puzzle/assets/sounds/moving.mp3';
         this.puzzleField.className = 'puzzle';
         this.time.className = 'time';
         this.moves.className = 'moves';
@@ -257,7 +257,7 @@ export default class Field {
             <label class="size-option__label">Field size</label>
             <select class="size-option__select">
                     <option value="9" class="select-option">3x3</option>
-                    <option value="16" class="select-option">4x4</option>
+                    <option value="16" class="select-option" selected>4x4</option>
                     <option value ="25" class ="select-option">5x5</option>
                     <option value="36" class="select-option">6x6</option>
                     <option value="49" class="select-option">7x7</option>
