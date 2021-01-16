091cd7e496e3b9f8c055dcf3af00642581d1949a
Tasty <sapegin.petya@yandex.ru>
2021-01-16 23:15:53 +0400

    refactor: update webpack plugins

--- webpack.config.js
+++ webpack.config.js
@@ -1,58 +1,69 @@
 const path = require('path');
-const HTMLWebpackPlugin = require('html-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const loader = require('sass-loader');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
-    entry: './gem-puzzle/src/app.js',
-    output: {
-        path: path.resolve(__dirname, 'gem-puzzle/dist'),
-        filename: 'bundle.js'
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-                template: './gem-puzzle/src/index.html'
-            }
-
-        ),
-        new MiniCssExtractPlugin({
-            filename: 'style.css',
-        })
+  entry: './gem-puzzle/src/app.js',
+  output: {
+    path: path.resolve(__dirname, 'gem-puzzle/dist'),
+    filename: 'bundle.js',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './gem-puzzle/src/index.html',
+    }),
+    new MiniCssExtractPlugin({
+      filename: 'style.css',
+    }),
+    new CleanWebpackPlugin(),
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: path.resolve(__dirname, 'gem-puzzle/assets/'),
+          to: path.resolve(__dirname, 'gem-puzzle/dist'),
+        },
+      ],
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              reloadAll: true,
+            },
+          },
+          'css-loader',
+          'sass-loader',
+        ],
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|jpg|svg)$/,
+        use: 'file-loader',
+      },
+      {
+        test: /\.ttf$/,
+        type: 'asset/inline',
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: ['babel-loader', 'eslint-loader'],
+      },
     ],
-    module: {
-        rules: [{
-                test: /\.scss$/,
-                use: [{
-                        loader: MiniCssExtractPlugin.loader,
-                        options: {
-                            reloadAll: true
-                        },
-                    },
-                    'css-loader',
-                    'sass-loader'
-                ]
-            },
-            {
-                test: /\.css$/,
-                use: ['style-loader', 'css-loader']
-            },
-            {
-                test: /\.(png|jpg|svg)$/,
-                use: ['file-loader']
-            },
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use: ['babel-loader', 'eslint-loader']
-            }
-        ]
-
-    },
-    devServer: {
-        port: 3000,
-        open: true,
-        overlay: true
-    }
+  },
+  devServer: {
+    port: 3000,
+    open: true,
+    overlay: true,
+  },
 };
